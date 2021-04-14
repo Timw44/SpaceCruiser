c0178528447c65e00b0f256ff969f95f1a08d537
Timothy Wilks <91078140@mvusd.net>
2021-04-13 17:36:32 -0700

    setup game activity and highScore activity

--- app/src/main/java/com/example/spacecruiser/GameScreen.java
+++ app/src/main/java/com/example/spacecruiser/GameScreen.java
@@ -1,8 +1,14 @@
 package com.example.spacecruiser;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GameScreen extends AppCompatActivity
 {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.game);
+    }
 }
--- app/src/main/java/com/example/spacecruiser/HighscoreScreen.java
+++ app/src/main/java/com/example/spacecruiser/HighscoreScreen.java
@@ -1,8 +1,14 @@
 package com.example.spacecruiser;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class HighscoreScreen extends AppCompatActivity
 {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.highscore);
+    }
 }
