d8827e20cdb448edab328b60e139df1cc45ca4a0
Timothy Wilks <91078140@mvusd.net>
2021-04-13 17:45:16 -0700

    added back to main menu btn to Game activity

--- app/src/main/java/com/example/spacecruiser/GameScreen.java
+++ app/src/main/java/com/example/spacecruiser/GameScreen.java
@@ -1,6 +1,7 @@
 package com.example.spacecruiser;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +11,10 @@ public class GameScreen extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
+    }
+
+    public void backToMainMenu(View view)
+    {
+        setContentView(R.layout.activity_main);
     }
 }
