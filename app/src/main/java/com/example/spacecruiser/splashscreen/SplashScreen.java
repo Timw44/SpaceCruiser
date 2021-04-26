c6e58c6c18dc51ffeb93698d860d2e75c814bd67
Timothy Wilks <91078140@mvusd.net>
2021-04-26 12:44:20 -0700

    setup splash screen activity

--- app/src/main/java/com/example/spacecruiser/splashscreen/SplashScreen.java
+++ app/src/main/java/com/example/spacecruiser/splashscreen/SplashScreen.java
@@ -1,4 +1,17 @@
 package com.example.spacecruiser.splashscreen;
 
-public class SplashScreen {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spacecruiser.R;
+
+public class SplashScreen extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+        getSupportActionBar().hide();
+    }
 }
