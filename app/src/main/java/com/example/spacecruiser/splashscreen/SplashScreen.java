ce9e68c9371bd69d2def90525e5f11bbf2786df1
Timothy Wilks <91078140@mvusd.net>
2021-04-26 13:02:59 -0700

    implemented the method for the splash screen

--- app/src/main/java/com/example/spacecruiser/splashscreen/SplashScreen.java
+++ app/src/main/java/com/example/spacecruiser/splashscreen/SplashScreen.java
@@ -1,9 +1,12 @@
 package com.example.spacecruiser.splashscreen;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.spacecruiser.MainActivity;
 import com.example.spacecruiser.R;
 
 public class SplashScreen extends AppCompatActivity
@@ -13,5 +16,15 @@ public class SplashScreen extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
         getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+
+        }, 3*1000); // wait for 3 seconds
     }
 }
