ad3884b786cdef8b01873212c5092498533adeed
Timothy Wilks <91078140@mvusd.net>
2021-05-02 11:52:25 -0700

    made ship appear once game starts

--- app/src/main/java/com/example/spacecruiser/GameScreen.java
+++ app/src/main/java/com/example/spacecruiser/GameScreen.java
@@ -4,15 +4,19 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.spacecruiser.splashscreen.SplashScreen;
 
 public class GameScreen extends AppCompatActivity
 {
     Button beginBtn = (Button) findViewById(R.id.beginBtn);
     Button backBtn = (Button) findViewById(R.id.backGameBtn);
     TextView intro = (TextView) findViewById(R.id.introTX);
+    ImageView ship = (ImageView) findViewById(R.id.SpaceCruiser);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +31,7 @@ public class GameScreen extends AppCompatActivity
         beginBtn.setVisibility(View.GONE);
         backBtn.setVisibility(View.GONE);
         intro.setVisibility(View.GONE);
+        ship.setVisibility(View.VISIBLE);
     }
 
     public void backToMainMenu(View view)
