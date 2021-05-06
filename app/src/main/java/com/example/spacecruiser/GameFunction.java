8af77588ffb8d63661604501918077d44f965dea
Timothy Wilks <91078140@mvusd.net>
2021-05-05 20:35:56 -0700

    added method to move the ship

--- app/src/main/java/com/example/spacecruiser/GameFunction.java
+++ app/src/main/java/com/example/spacecruiser/GameFunction.java
@@ -2,11 +2,12 @@ package com.example.spacecruiser;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 
 public class GameFunction
 {
-
+private ImageView ship;
 
     public void playerMove()
     {
--- app/src/main/java/com/example/spacecruiser/GameScreen.java
+++ app/src/main/java/com/example/spacecruiser/GameScreen.java
@@ -1,10 +1,14 @@
 package com.example.spacecruiser;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,6 +21,9 @@ public class GameScreen extends AppCompatActivity
     Button backBtn = (Button) findViewById(R.id.backGameBtn);
     TextView intro = (TextView) findViewById(R.id.introTX);
     ImageView ship = (ImageView) findViewById(R.id.SpaceCruiser);
+    private ViewGroup mainLayout = (RelativeLayout) findViewById(R.id.main);
+    private int xDelta;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +39,38 @@ public class GameScreen extends AppCompatActivity
         backBtn.setVisibility(View.GONE);
         intro.setVisibility(View.GONE);
         ship.setVisibility(View.VISIBLE);
+        ship.setOnTouchListener(onTouchListener());
+    }
+
+
+//moves player back and forth
+    private View.OnTouchListener onTouchListener() {
+        return new View.OnTouchListener() {
+            @SuppressLint("ClickableViewAccessibility")
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                final int x = (int) event.getRawX();
+
+                switch (event.getAction() & MotionEvent.ACTION_MASK) {
+
+                    case MotionEvent.ACTION_DOWN:
+                        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams)
+                                view.getLayoutParams();
+                        xDelta = x - lParams.leftMargin;
+                        break;
+
+                    case MotionEvent.ACTION_MOVE:
+                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
+                        layoutParams.leftMargin = x - xDelta;
+                        layoutParams.rightMargin = 0;
+                        layoutParams.bottomMargin = 0;
+                        view.setLayoutParams(layoutParams);
+                        break;
+                }
+                mainLayout.invalidate();
+                return true;
+            }
+        };
     }
 
     public void backToMainMenu(View view)
