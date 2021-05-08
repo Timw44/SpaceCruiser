bf8c6e65d1453c8cfb721e4852fd9f9132028d46
Timothy Wilks <91078140@mvusd.net>
2021-05-08 12:42:10 -0700

    redid methods to start the game

--- app/src/main/java/com/example/spacecruiser/GameFunction.java
+++ app/src/main/java/com/example/spacecruiser/GameFunction.java
@@ -1,17 +1,48 @@
 package com.example.spacecruiser;
 
+import android.content.Context;
+import android.view.MotionEvent;
+import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
+import androidx.appcompat.app.AppCompatActivity;
 
-public class GameFunction
+
+public class GameFunction extends SurfaceView implements Runnable
 {
-private ImageView ship;
+    public static boolean isPlaying = false;
+    private boolean isGameOver, isMoving = false;
+    private int screenX, score = 0;
 
-    public void playerMove()
-    {
-
+    public GameFunction(Context context) {
+        super(context);
     }
 
+    @Override
+    public void run() {
+        while(isPlaying)
+        {
+
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if (event.getX() < screenX / 2) {
+                   isMoving = true;
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                isMoving = false;
+                if (event.getX() > screenX / 2)
+                    //toShoot++;
+                break;
+        }
+        return true;
+    }
 }
--- app/src/main/java/com/example/spacecruiser/GameScreen.java
+++ app/src/main/java/com/example/spacecruiser/GameScreen.java
@@ -12,6 +12,7 @@ import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.spacecruiser.splashscreen.SplashScreen;
 
@@ -21,7 +22,7 @@ public class GameScreen extends AppCompatActivity
     Button backBtn = (Button) findViewById(R.id.backGameBtn);
     TextView intro = (TextView) findViewById(R.id.introTX);
     ImageView ship = (ImageView) findViewById(R.id.SpaceCruiser);
-    private ViewGroup mainLayout = (RelativeLayout) findViewById(R.id.main);
+    private ViewGroup mainLayout = (ConstraintLayout) findViewById(R.id.game);
     private int xDelta;
 
 
@@ -39,38 +40,7 @@ public class GameScreen extends AppCompatActivity
         backBtn.setVisibility(View.GONE);
         intro.setVisibility(View.GONE);
         ship.setVisibility(View.VISIBLE);
-        ship.setOnTouchListener(onTouchListener());
-    }
-
-
-//moves player back and forth
-    private View.OnTouchListener onTouchListener() {
-        return new View.OnTouchListener() {
-            @SuppressLint("ClickableViewAccessibility")
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                final int x = (int) event.getRawX();
-
-                switch (event.getAction() & MotionEvent.ACTION_MASK) {
-
-                    case MotionEvent.ACTION_DOWN:
-                        RelativeLayout.LayoutParams lParams = (RelativeLayout.LayoutParams)
-                                view.getLayoutParams();
-                        xDelta = x - lParams.leftMargin;
-                        break;
-
-                    case MotionEvent.ACTION_MOVE:
-                        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) view.getLayoutParams();
-                        layoutParams.leftMargin = x - xDelta;
-                        layoutParams.rightMargin = 0;
-                        layoutParams.bottomMargin = 0;
-                        view.setLayoutParams(layoutParams);
-                        break;
-                }
-                mainLayout.invalidate();
-                return true;
-            }
-        };
+        GameFunction.isPlaying = true;
     }
 
     public void backToMainMenu(View view)
