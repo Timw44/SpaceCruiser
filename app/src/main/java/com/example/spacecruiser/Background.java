081c1eed1e9fd7fc84a5b0818af86fe3dfe5e8f4
Timothy Wilks <91078140@mvusd.net>
2021-05-09 11:42:24 -0700

    made background method to make a plain that can move the background

--- app/src/main/java/com/example/spacecruiser/Background.java
+++ app/src/main/java/com/example/spacecruiser/Background.java
@@ -1,5 +1,20 @@
 package com.example.spacecruiser;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 public class Background
 {
+    //variables
+    int x, y = 0;
+    Bitmap background;
+
+    //makes a plain that the background can move on
+    Background(int screenX, int screenY, Resources res) {
+
+        background = BitmapFactory.decodeResource(res, R.drawable.space);
+        background = Bitmap.createScaledBitmap(background, screenX, screenY, false);
+
+    }
 }
