fe02c1578aace2e70655fc3eab0884b516181298
Timothy Wilks <91078140@mvusd.net>
2021-05-11 14:49:45 -0700

    made a method to assign a player ship and make it size with the screen

--- app/src/main/java/com/example/spacecruiser/Player.java
+++ app/src/main/java/com/example/spacecruiser/Player.java
@@ -5,7 +5,31 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
 
+import static com.example.spacecruiser.GameFunction.screenRatioX;
+import static com.example.spacecruiser.GameFunction.screenRatioY;
+
 public class Player
 {
+    int x, y, width, height;
+    Bitmap spaceShip;
+
+    Player(int screenY, Resources res)
+    {
+        spaceShip = BitmapFactory.decodeResource(res, R.drawable.spacecrusier);
+
+        width = spaceShip.getWidth();
+        height = spaceShip.getHeight();
+
+        width /= 4;
+        height /= 4;
+
+        width = (int) (width * screenRatioX);
+        height = (int) (height * screenRatioY);
+
+        spaceShip = Bitmap.createScaledBitmap(spaceShip, width, height, false);
+
+        y = screenY / 2;
+        x = (int) (64 * screenRatioX);
+    }
 
 }
