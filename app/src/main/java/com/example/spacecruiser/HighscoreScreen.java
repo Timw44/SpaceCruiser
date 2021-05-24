1d4d0b84555ec439e446715c6282d28e2623e222
Timothy Wilks <91078140@mvusd.net>
2021-05-23 23:09:32 -0700

    leader board gets filled

--- app/src/main/java/com/example/spacecruiser/HighscoreScreen.java
+++ app/src/main/java/com/example/spacecruiser/HighscoreScreen.java
@@ -22,9 +22,10 @@ import java.util.ArrayList;
 public class HighscoreScreen extends AppCompatActivity
 {
     //variables
-    //TextView first, second, third, fourth, fifth, sixth, seventh, eighth, ninth, tenth;
+    TextView firstK, secondK, thirdK, fourthK, fifthK, sixthK, seventhK, eighthK, ninthK, tenthK,
+             firstV, secondV, thirdV, fourthV, fifthV, sixthV, seventhV, eighthV, ninthV, tenthV;
     TextView[] positions = new TextView[10];
-    String name, score;
+    TextView[] names = new TextView[10];
     //ArrayList<String> keys;
     FirebaseDatabase database;
     DatabaseReference reference;
@@ -47,38 +48,48 @@ public class HighscoreScreen extends AppCompatActivity
     {
         setPositions();
 
-        /*first = (TextView) findViewById(R.id.scoreTX1);
-        second = (TextView) findViewById(R.id.scoreTX2);
-        third = (TextView) findViewById(R.id.scoreTX3);
-        fourth = (TextView) findViewById(R.id.scoreTX4);
-        fifth = (TextView) findViewById(R.id.scoreTX5);
-        sixth = (TextView) findViewById(R.id.scoreTX6);
-        seventh = (TextView) findViewById(R.id.scoreTX7);
-        eighth = (TextView) findViewById(R.id.scoreTX8);
-        ninth = (TextView) findViewById(R.id.scoreTX9);
-        tenth = (TextView) findViewById(R.id.scoreTX10);*/
+        /*firstK = (TextView) findViewById(R.id.scoreTX1Key);
+        secondK = (TextView) findViewById(R.id.scoreTX2Key);
+        thirdK = (TextView) findViewById(R.id.scoreTX3Key);
+        fourthK = (TextView) findViewById(R.id.scoreTX4Key);
+        fifthK = (TextView) findViewById(R.id.scoreTX5Key);
+        sixthK = (TextView) findViewById(R.id.scoreTX6Key);
+        seventhK = (TextView) findViewById(R.id.scoreTX7Key);
+        eighthK = (TextView) findViewById(R.id.scoreTX8Key);
+        ninthK = (TextView) findViewById(R.id.scoreTX9Key);
+        tenthK = (TextView) findViewById(R.id.scoreTX10Key);
+        firstV = (TextView) findViewById(R.id.scoreTX1Value);
+        secondV = (TextView) findViewById(R.id.scoreTX2Value);
+        thirdV = (TextView) findViewById(R.id.scoreTX3Value);
+        fourthV = (TextView) findViewById(R.id.scoreTX4Value);
+        fifthV = (TextView) findViewById(R.id.scoreTX5Value);
+        sixthV = (TextView) findViewById(R.id.scoreTX6Value);
+        seventhV = (TextView) findViewById(R.id.scoreTX7Value);
+        eighthV = (TextView) findViewById(R.id.scoreTX8Value);
+        ninthV = (TextView) findViewById(R.id.scoreTX9Value);
+        tenthV = (TextView) findViewById(R.id.scoreTX10Value);*/
 
         //Query queryRef = reference.orderByKey().limitToLast(10);
 
         reference.orderByKey().limitToLast(10).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                sort(Integer.parseInt(snapshot.getKey()));
+                sort(Integer.parseInt(snapshot.getKey()), snapshot.getValue().toString());
             }
 
             @Override
             public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                sort(Integer.parseInt(snapshot.getKey()));
+                sort(Integer.parseInt(snapshot.getKey()), snapshot.getValue().toString());
             }
 
             @Override
             public void onChildRemoved(@NonNull DataSnapshot snapshot) {
-                sort(Integer.parseInt(snapshot.getKey()));
+                sort(Integer.parseInt(snapshot.getKey()), snapshot.getValue().toString());
             }
 
             @Override
             public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
-                sort(Integer.parseInt(snapshot.getKey()));
+                sort(Integer.parseInt(snapshot.getKey()), snapshot.getValue().toString());
             }
 
             @Override
@@ -113,26 +124,38 @@ public class HighscoreScreen extends AppCompatActivity
 
     public void setPositions()
     {
-        positions[0] = findViewById(R.id.scoreTX1);
-        positions[1] = findViewById(R.id.scoreTX2);
-        positions[2] = findViewById(R.id.scoreTX3);
-        positions[3] = findViewById(R.id.scoreTX4);
-        positions[4] = findViewById(R.id.scoreTX5);
-        positions[5] = findViewById(R.id.scoreTX6);
-        positions[6] = findViewById(R.id.scoreTX7);
-        positions[7] = findViewById(R.id.scoreTX8);
-        positions[8] = findViewById(R.id.scoreTX9);
-        positions[9] = findViewById(R.id.scoreTX10);
+        positions[0] = findViewById(R.id.scoreTX1Key);
+        positions[1] = findViewById(R.id.scoreTX2Key);
+        positions[2] = findViewById(R.id.scoreTX3Key);
+        positions[3] = findViewById(R.id.scoreTX4Key);
+        positions[4] = findViewById(R.id.scoreTX5Key);
+        positions[5] = findViewById(R.id.scoreTX6Key);
+        positions[6] = findViewById(R.id.scoreTX7Key);
+        positions[7] = findViewById(R.id.scoreTX8Key);
+        positions[8] = findViewById(R.id.scoreTX9Key);
+        positions[9] = findViewById(R.id.scoreTX10Key);
+        names[0] = findViewById(R.id.scoreTX1Value);
+        names[1] = findViewById(R.id.scoreTX2Value);
+        names[2] = findViewById(R.id.scoreTX3Value);
+        names[3] = findViewById(R.id.scoreTX4Value);
+        names[4] = findViewById(R.id.scoreTX5Value);
+        names[5] = findViewById(R.id.scoreTX6Value);
+        names[6] = findViewById(R.id.scoreTX7Value);
+        names[7] = findViewById(R.id.scoreTX8Value);
+        names[8] = findViewById(R.id.scoreTX9Value);
+        names[9] = findViewById(R.id.scoreTX10Value);
     }
 
-    public void sort(int value)
+    public void sort(int value, String name)
     {
-        for (int i = 0; i<positions.length; i++)
+        for (int i = 0; i<positions.length-1; i++)
         {
-
             if(value > Integer.parseInt(positions[i].getText().toString()))
             {
-                positions[i].setText((i+1) + ". " + value);
+                positions[i+1].setText(positions[i].getText());
+                names[i+1].setText(names[i].getText());
+                positions[i].setText(value + "");
+                names[i].setText(name);
             }
         }
     }
