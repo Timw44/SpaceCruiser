9f7b550567bbc83a9a8ff55189821333b0d954f9
Timothy Wilks <91078140@mvusd.net>
2021-05-24 17:31:21 -0700

    added a few collision tests

--- app/src/test/java/com/example/spacecruiser/ExampleUnitTest.java
+++ app/src/test/java/com/example/spacecruiser/ExampleUnitTest.java
@@ -1,5 +1,7 @@
 package com.example.spacecruiser;
 
+import android.graphics.Rect;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,8 +12,35 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void collisionXSame()
+    {
+        Rect enemy = new Rect(1,5,1,3);
+        Rect player = new Rect(1,1,1,1);
+
+        assertEquals(false, Rect.intersects(enemy, player));
+    }
+    @Test
+    public void collisionYSame()
+    {
+        Rect enemy = new Rect(5,1,4,1);
+        Rect player = new Rect(1,1,1,1);
+
+        assertEquals(false, Rect.intersects(enemy, player));
+    }
+    @Test
+    public void collision()
+    {
+        Rect enemy = new Rect(1,1,1,1);
+        Rect player = new Rect(1,1,1,1);
+
+        assertEquals(true, Rect.intersects(enemy, player));
+    }
+
+    @Test
+    public void test()
+    {
+
     }
 }
